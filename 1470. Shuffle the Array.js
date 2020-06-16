23709fce33601a587117456d303892a501eb83bf
Robin Aguilar <robin.aguilar@gmail.com>
2020-06-16 14:31:55 -0500

    First Commit

---
+++ 1470. Shuffle the Array.js
@@ -0,0 +1,54 @@
+// Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+// Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,5,1,3,4,7], n = 3
+// Output: [2,3,5,4,1,7] 
+// Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+// Example 2:
+
+// Input: nums = [1,2,3,4,4,3,2,1], n = 4
+// Output: [1,4,2,3,3,2,4,1]
+// Example 3:
+
+// Input: nums = [1,1,2,2], n = 2
+// Output: [1,2,1,2]
+ 
+
+// Constraints:
+
+// 1 <= n <= 500
+// nums.length == 2n
+// 1 <= nums[i] <= 10^3
+// Accepted
+// 23,668
+// Submissions
+// 26,300
+
+
+//____________________________________________________________________________________
+// Solution:
+
+function shuffle(nums, n){
+    let arr = [nums[0], nums[n]];
+    for (let i = 1; i < n; i++){
+        arr.push(nums[i]);
+        arr.push(nums[i+n]);
+    }
+    
+    return arr
+}
+
+// Accepted
+// Runtime: 64 ms
+// Your input
+// [2,5,1,3,4,7]
+// 3
+// Output
+// [2,3,5,4,1,7]
+// Expected
+// [2,3,5,4,1,7]
